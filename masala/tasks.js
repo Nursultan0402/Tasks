0d86b538716886f76ea9990e90257099696a1e47
Nursulton <nursulton6669@gmail.com>
2023-08-26 18:39:50 +0500

    masala

---
+++ masala/tasks.js
@@ -0,0 +1,67 @@
+// sum % 2 !== 0
+
+// let num = 0;
+//  if(num == 0){
+//     console.log('Toqham Juftham emas');
+//  }
+//  else if(num % 2 == 0) {
+//     console.log('Juft son');
+// } else {
+//     console.log('Toq son');
+// } 
+
+// let num = 0
+// switch (num % 2) {
+//     case 0: console.log('Juft son');break
+//     case 1: console.log('Toq son');
+// }
+
+// let num = 91
+// let sum = num % 2 === 0 ? console.log('Juft son') : console.log('Toq son'); 
+
+// let num = 11
+// let sum = num % 2 === 0 ? 'Juft son' : 'Toq son'; console.log(sum);
+
+// ////////         2         ///////////////////
+
+// prompt('')
+
+// let ball = 12
+// if( ball > 85 && ball <= 100){
+//     console.log('A');
+// }else if(ball > 60 &&  ball <=86){
+//     console.log('B');
+// }else if(ball > 45 && ball <= 60){
+//     console.log('C');
+// }else if(ball >= 0 && ball <= 45){
+//     console.log('Failed');
+// }else{
+//     console.log('Ortiqcha raqam kiritildi');
+// }
+
+// ////////////         3        ////////////
+
+// let meva = 'behi'
+// switch(meva){
+//     case 'olma': console.log('10 000');break
+//     case 'nok' : console.log('12 000');break
+//     case 'tarvuz': console.log('20 000');break
+//     default: console.log('unday meva yoq');
+// }
+
+// let meva = 'tarvuz'
+// if (meva == 'olma') console.log('10 000');
+// else if (meva == 'nok') console.log('12 000');
+// else if (meva == 'tarvuz') console.log('20 000');
+// else console.log('Bunday meva yoq');
+    
+// ////////       4         /////////
+
+// let num = 20
+// if(num % 3 == 0 && num % 5 == 0) {
+//     console.log('3 gaham 5 gaham bolinadi');
+// }else if (num % 3 == 0) {
+//     console.log('3 ga bolinadi');
+// }else if (num % 5 == 0) {
+//     console.log('5 ga bolinadi');
+// }else console.log('3 gaham 5 gaham bolinmaydi');
